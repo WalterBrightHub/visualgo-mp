8ccd734788948c030aea1fedb78a2dcb65926fff
WalterBright <ssmbuild@outlook.com>
2021-04-16 02:00:22 +0800

    Create: init commit

---
+++ pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="content">
+    <view class="algorithm-list">
+      <navigator class="algorithm-block" v-for="algorithm in algorithms" :url="algorithm.url">
+        <view class="algo-name">{{algorithm.name}}</view>
+      </navigator>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        algorithms: [{
+          name: '排序',
+          detail: '冒泡、选择、插入、归并、快速等排序方法',
+          url: '/pages/sort/sort'
+        }]
+      }
+    },
+    onLoad() {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style lang="scss">
+  .content {}
+</style>
---
+++ pages/sort/sort.vue
@@ -0,0 +1,70 @@
+<template>
+  <view>
+    <view class="sort-type-picker-list">
+      <view class="sort-type" v-for="(sortType,index) in sortTypes"
+        :class="{['sort-type-selected']:index===sortTypeValue}" @tap="onChangeSortType(index)">{{sortType.name}}</view>
+    </view>
+    <view class="canvas">
+      <view class="rect"></view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        sortTypes: [{
+            name: '冒泡',
+            id: 'bubble'
+          },
+          {
+            name: '选择',
+            id: 'selection'
+          },
+          {
+            name: '插入',
+            id: 'insertion'
+          },
+          {
+            name: '归并',
+            id: 'merge'
+          },
+          {
+            name: '快速',
+            id: 'quick'
+          },
+        ],
+        sortTypeValue: 0,
+        rects: []
+      }
+    },
+    methods: {
+      onChangeSortType(index) {
+        if (index !== this.sortTypeValue) {
+          this.sortTypeValue = index
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .sort-type-picker-list {
+    display: flex;
+    // justify-content: space-between;
+  }
+
+  .sort-type {
+    font-size: 14px;
+    padding: 20rpx;
+    flex: 1;
+    display: flex;
+    justify-content: center;
+  }
+
+  .sort-type-selected {
+    color: #fff;
+    background-color: $algo-theme-color;
+  }
+</style>
